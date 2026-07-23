5a3a7abfbbb2b66c8821008340e2302a205ac484
ducanh <anhductranminh@gmail.com>
2026-07-24 02:20:35 +0700

    Player_StateMachine

---
+++ Assets/Scrips/Player_SC/PlayerStates/SuperState/PlayerAbilityState.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilityState : PlayerState
+{
+    protected bool isAbilityDone;
+
+    private bool isGounded;
+    public PlayerAbilityState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+    {
+    }
+    public override void DoChecks()
+    {
+        base.DoChecks();
+
+        isGounded = player.CheckIfGrounded();
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        isAbilityDone = false;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+        if (isAbilityDone)
+        {
+            if (isGounded && player.CurrentVelocity.y < 0.01f)
+            {
+                stateMachine.ChangeState(player.IdleState);
+            }
+            else
+            {
+                stateMachine.ChangeState(player.InAirState);
+            }
+        }
+    }
+
+    public override void PhysicsUpdate()
+    {
+        base.PhysicsUpdate();
+    }
+}
